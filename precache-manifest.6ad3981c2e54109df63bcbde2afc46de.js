965175f8f3000acf9cbdb75ce79504859c169438
Pavlo <pashapshenishnyuk@gmail.com>
2020-03-11 19:41:46 +0200

    Updates

---
+++ precache-manifest.6ad3981c2e54109df63bcbde2afc46de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf578752731b2b82f50dea71980dff3d",
+    "url": "/StarWars-DB/index.html"
+  },
+  {
+    "revision": "3d387cf9dbda29235d4e",
+    "url": "/StarWars-DB/static/css/main.f83f171b.chunk.css"
+  },
+  {
+    "revision": "b0a2fc472ed36900f4a1",
+    "url": "/StarWars-DB/static/js/2.fed64497.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/StarWars-DB/static/js/2.fed64497.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d387cf9dbda29235d4e",
+    "url": "/StarWars-DB/static/js/main.179399e8.chunk.js"
+  },
+  {
+    "revision": "b8d382bbaaafcd13fe11",
+    "url": "/StarWars-DB/static/js/runtime-main.75618d17.js"
+  }
+]);
